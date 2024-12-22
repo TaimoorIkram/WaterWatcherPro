f9c6765e911917210bef11526db24e12e7cbbe89
Syed Abdulrab <syed.abdulrab.cowlar@gmail.com>
2024-12-23 01:01:18 +0500

    fix: incorrect controller method name

--- backend/routes/householdRoutes.js
+++ backend/routes/householdRoutes.js
@@ -4,7 +4,7 @@ const authenticateToken = require('../middlewares/jwt.middleware');
 
 const router = express.Router();
 
-router.get('/', authenticateToken,householdController.getHouseholds);
+router.get('/', authenticateToken,householdController.getAllHouseholds);
 router.post('/', authenticateToken,householdController.createHousehold);
 router.put('/:id', authenticateToken,householdController.updateHousehold);
 router.delete('/:id',authenticateToken, householdController.deleteHousehold);
