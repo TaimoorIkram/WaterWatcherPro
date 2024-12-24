1d421094c4d1ff904f9515b6703395b912590e2e
AhsanSajjad322 <ahsansajjad57135@gmail.com>
2024-12-24 17:56:57 +0500

    Update db structure and data

--- backend/config/db.js
+++ backend/config/db.js
@@ -10,28 +10,10 @@ const dbPath = path.join(__dirname, "db.json");
 if (!fs.existsSync(dbPath)) {
   console.log("Database file does not exist. Initializing with defaults...");
   const initialData = {
-    Household: [],
-    Requests: [],
-    User: [],
-    Sensor: [],
-    HouseholdConfig: [
-      {
-        "id": 1,
-        "household_id": 1,
-        "user_id": "5dd82a87-ad2e-42ce-8218-86f9313ef009",
-        "tank_height": 2,
-        "tank_capacity": 400,
-        "peak_usage_hours": "12-13",
-        "water_availability_hours": "12-20",
-        "min_threshold_normal_hours": 20,
-        "min_threshold_peak_hours": 10,
-        "max_threshold_normal_hours": 100,
-        "max_threshold_peak_hours": 7,
-        "sensor_id": 1,
-        "actuator_id": 2
-      }
+    User: [
+      { id: 3, name: "admin", email: "admin@gmail.com", password: "admin", roleId: 2},
+      { id: 4, name: "user", email: "user@gmail.com", password: "user", roleId: 3}
     ],
-    SensorData: [],
     Roles: [
       { id: 1, name: "super_admin" },
       { id: 2, name: "admin" },
@@ -40,8 +22,8 @@ if (!fs.existsSync(dbPath)) {
     ],
     Requests:[],
     Devices: [],
-    ActuatorData:[],
-
+    SensorData: [],
+    Nounces:[],
   };
   fs.writeFileSync(dbPath, JSON.stringify(initialData, null, 2), "utf-8");
 } else {
